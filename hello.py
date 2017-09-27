f54a38d4b16c5b5f7eb9a2e4cd07f3a1ed3e7ce8
grit0 <krit405f@gmail.com>
2017-09-27 16:39:50 +0700

    firest and request

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 """Cloud Foundry test"""
 from flask import Flask
 import os
-
+import pyrebase
+config = {
+  "apiKey": "AIzaSyCSEsN8V7vjKrEBB0sjgoZs8aG-TLd-suI",
+  "authDomain": "forexbygrit.firebaseapp.com",
+  "databaseURL": "https://forexbygrit.firebaseio.com",
+  "storageBucket": "forexbygrit.appspot.com"
+}
+firebase = pyrebase.initialize_app(config)
+import requests 
+r = requests.get('https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD%2CGBPJPY%2CAUDUSD&api_key=o7t3pexnQ7NygogAKovIYhQ362UcmwGM')
 app = Flask(__name__)
 
 port = int(os.getenv("PORT"))
@@ -9,6 +18,12 @@ port = int(os.getenv("PORT"))
 @app.route('/')
 def hello_world():
     return 'Hello World! I am running on port ' + str(port)
+
+@app.route('/add')
+def add():
+    db = firebase.database()
+    db.child().push(r.json())
+    return "add finish"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
